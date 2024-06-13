d39e03d8b23b95c0b8bdde5455c5ca6af2e8ebc4
Fabio <fabio@f2sc.com>
2024-06-13 09:45:56 +0200

    Feat: Añadidos modelos de Tareas y Categorias. Añadido también la clase TareaContext

---
+++ Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace prrojectEF.Models
+{
+    public class Categoria
+    {
+        public Guid CategoriaID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public virtual ICollection<Tarea> Tareas { get; set; }
+    }
+}
---
+++ TareasContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using prrojectEF.Models;
+
+namespace prrojectEF
+{
+    public class TareasContext:DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Tarea> Tareas { get; set; }
+
+        public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
+    }
+}
